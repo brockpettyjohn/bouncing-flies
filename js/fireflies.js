0be17d4c802de35b5652475b26f2318f6e09a30a
Brock Pettyjohn <brockpettyjohn@gmail.com>
2017-11-30 16:17:08 -0700

    just ate a corndog

--- js/fireflies.js
+++ js/fireflies.js
@@ -16,19 +16,34 @@ const GameState = {
         this.scale.pageAlignVertically = true
         this.background = this.game.add.sprite(0, 0, 'background')
         this.firefly = placeSprite(this.game.world.randomX, this.game.world.randomY, 'firefly', .15, .5)
+        this.firefly.inputEnabled = true
+        this.firefly.input.onHold = true
+        this.firefly.events.onInputDown.add(() => {this.flyAway(fireflyObj, this.firefly)}, this)
         this.firefly2 = placeSprite(this.game.world.randomX, this.game.world.randomY, 'firefly', .15, .5)
         this.firefly3 = placeSprite(this.game.world.randomX, this.game.world.randomY, 'firefly', .15, .5)
         this.firefly4 = placeSprite(this.game.world.randomX, this.game.world.randomY, 'firefly', .15, .5)
         this.firefly5 = placeSprite(this.game.world.randomX, this.game.world.randomY, 'firefly', .15, .5)
 
+        const fireflyObj = [{
+            name: this.firefly
+        },
+        {
+            name: this.firefly2
+        },
+        {
+            name: this.firefly3
+        },
+        {
+            name: this.firefly4
+        },
+        {
+            name: this.firefly5
+        }]
+
         this.game.physics.startSystem(Phaser.Physics.Arcade)
         this.game.physics.enable([this.firefly, this.firefly2, this.firefly3, this.firefly3, this.firefly4, this.firefly5], Phaser.Physics.ARCADE)
         this.game.physics.arcade.collide(this.firefly, this.firefly2, this.firefly3, this.firefly4, this.firefly5)
-        bounceHouse(this.firefly)
-        bounceHouse(this.firefly2)
-        bounceHouse(this.firefly3)
-        bounceHouse(this.firefly4)
-        bounceHouse(this.firefly5)
+        bounceHouse(fireflyObj)
 
         this.firefly.addChild(makeChild(0, 0, 'banana', 1.25))
         this.firefly2.addChild(makeChild(0, 0, 'apple', 1.25))
@@ -48,6 +63,19 @@ const GameState = {
         this.game.physics.arcade.collide(this.firefly3, this.firefly4);
         this.game.physics.arcade.collide(this.firefly3, this.firefly5);
         this.game.physics.arcade.collide(this.firefly4, this.firefly5);
+    },
+    flyAway: function (arr, clickedFirefly) {
+        
+        arr.map(element => {
+            console.log(element.name)
+            element.name.body.velocity.setTo(0)
+            if(element.name != clickedFirefly){
+                const laterGator = game.add.tween(element.name)
+                laterGator.to({y: -550 - element.name.height}, 500)
+                laterGator.start()
+                element.name.body.collideWorldBounds = false;
+            }
+        });
     }
 }
 
@@ -65,13 +93,16 @@ function makeChild(x, y, name, scale = 1) {
     return childSprite
 }
 
-
-
-function bounceHouse(name) {
-    name.body.velocity.setTo(50, 50);
-    name.body.collideWorldBounds = true;
-    name.body.bounce.setTo(1, 1)
+function bounceHouse(arr) {
+    arr.map(element => {
+        element.name.body.velocity.setTo(75, 75);
+        element.name.body.collideWorldBounds = true;
+        element.name.body.bounce.setTo(1, 1)
+    })
+    
 }
+
+
 
 game.state.add('GameState', GameState)
 game.state.start('GameState')
